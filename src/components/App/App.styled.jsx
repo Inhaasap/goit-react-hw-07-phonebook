75aa1206fbab2e9d92bae1841123e2db2ee3228f
Інга Юрченко <missingusya@hotmail.com>
2023-06-02 14:53:42 +0300

    Update App.styled.jsx

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,20 +1,31 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 300px;
-  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+
+  width: 400px;
+  max-width: 100%;
+
+  padding: 20px;
   margin: 10px auto;
+
   border-radius: 10px;
-  box-shadow: 0px 0px 10px #2a2a2a;
-  background-color: #fff;
+  box-shadow: inset 1px 1px 3px #8ba793, inset -1px -1px 5px #fff;
+  background-color: rgb(212, 239, 224);
 `;
 
 export const Title = styled.h1`
   text-align: center;
+  font-size: 30px;
 `;
 
 export const SubTitle = styled.h1`
-  text-align: center;
+ text-align: center;
+  font-size: 30px;
+  margin-top: 10px;
 `;
 
 export const LoaderContainer = styled.div`
